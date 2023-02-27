cd2193a0a5845be5d790f457c55fc50cfaf47843
Xinoi <zaunrithj@gmail.com>
2023-02-27 22:59:04 +0100

    changed to bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use bevy::prelude::*;
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_startup_system(setup_camera)
+        .insert_resource(ClearColor(Color::rgb(0.20, 0.33, 1.00)))
+        .add_plugins(DefaultPlugins)
+        .run();
+    
 }
