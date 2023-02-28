54b4d96c0ee82286ef498dd85868708ef0cac036
Xinoi <zaunrithj@gmail.com>
2023-02-28 18:18:45 +0100

    add window settings

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,27 @@ fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
+fn setup(commands: &mut Commands, asset_server: Res<AssetServer>, mut materials: ResMut<Assets<StandardMaterial>>) {
+    //Background Image
+    todo!();
+
+}
+
 fn main() {
     App::new()
         .add_startup_system(setup_camera)
         .insert_resource(ClearColor(Color::rgb(0.20, 0.33, 1.00)))
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(
+                WindowPlugin {
+                    window: WindowDescriptor {
+                        title: "DNAISC2".to_string(),
+                        width: 1200.,
+                        height: 800.,
+                        ..default()
+                    },
+                ..default()
+                }
+                ))
         .run();
     
 }
